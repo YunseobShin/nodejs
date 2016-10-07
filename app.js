9a3a3f505c418dfae077e63876e174ebbd889c0e
YunseobShin <skwi2004@gmail.com>
2016-10-08 00:59:28 +0900

    RESUful

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
-mongoose.connect("mongodb://y@ds053206.mlab.com:53206/ysmongo");
+mongoose.connect("mongodb://@ds053206.mlab.com:53206/ysmongo");
 var db = mongoose.connection;
 db.once("open", function() {
   console.log("DB connected");
@@ -17,24 +18,6 @@ db.on("error", function(err) {
   Collection: SQL의 테이블 개념, 이름은 항상 복수형으로(Users, Posts)
   Document: SQL의 column, object나 array도 담을 수 있어서 유연하지만 복잡함
 */
-
-//스키마, 자동으로 고유 id 생성됨
-var dataSchema = mongoose.Schema({
-  name: String,
-  count: Number
-});
-// 모델을 담는 변수는 대문자로 시작
-var Data = mongoose.model('data', dataSchema);
-Data.findOne({name:"myData"}, function(err, data) {
-  if(err) return console.log("Data ERROR: ", err);
-  if(!data) {
-    Data.create({name:"myData", count:0}, function(err, data) {
-      if(err) return console.log("Data ERROR:", err);
-      console.log("Counter initialized :", data);
-    });
-  }
-});
-
 /*
   mongoose.model()로 생성된 오브젝트로 사용하는 함수
     findOne(obj, callback 함수)
@@ -44,54 +27,57 @@ Data.findOne({name:"myData"}, function(err, data) {
     findOneAndRemove(obj, callback 함수)
 */
 
+// model setting
+var postSchema = mongoose.Schema({
+  title: {type:String, required:true},
+  body: {type:String, required:true},
+  createdAt: {type:Date, default:Date.now},
+  updatedAt: Date
+});
+var Post = mongoose.model('post', postSchema);
+
+// view setting
 app.set("view engine", "ejs");
+
+// set middlewares
 app.use(express.static(path.join(__dirname + '/public')));
+app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  Data.findOne({name:"myData"}, function(err, data) {
-    if(err) return console.log("Data ERROR: ", err);
-    data.count++;
-    data.save(function (err) {
-      if(err) return console.log("Data ERROR: ", err);
-      res.render("index", data);
-    });
+// set routes
+app.get('/posts', function(req, res) {
+  Post.find({}, function(err, posts) {
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, data:posts});
   });
-});
-
-app.get('/reset', function (req, res) {
-  setCounter(res, 0);
-});
-
-app.get('/set/count', function  (req, res) {
-  if(req.query.count) data.count=req.query.count;
-  else getCounter(res);
-});
-
-app.get('/set/:num', function (req, res) {
-  if(req.params.num) setCounter(res, req.params.num);
-  else getCounter(res);
-});
-
-function setCounter(res, num) {
-  console.log("setCounter");
-  Data.findOne({name:"myData"}, function(err, data){
-    if(err) return console.log("Data ERROR: ", err);
-    data.count=num;
-    data.save(function(err) {
-      if(err) return console.log("Data ERROR: ", err);
-      res.render("index", data);
-    });
+}); // index
+app.post('/posts', function(req, res) {
+  Post.create(req.body.post, function(err, post) {
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, data:post});
   });
-}
-
-function getCounter(res) {
-  console.log("getCounter");
-  Data.findOne({name:"myData"}, function(err, data) {
-    if(err) return console.log("Data ERROR: ", err);
-    res.render("index", data);
+}); // create
+app.get('/posts/:id', function(req, res) {
+  Post.findById(req.params.id, function(err, post) {
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, data:post});
   });
-}
+}); // show
+app.put('/posts/:id', function(req, res) {
+  req.body.post.updatedAt=Date.now();
+  Post.findByIdAndUpdate(req.params.id, req.body.post, function(err, post) {
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, message:post._id+" updated"});
+  });
+}); // update
+app.delete('/posts/:id', function(req, res) {
+  Post.findByIdAndRemove(req.params.id, function(err, post) {
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, message:req.params.id+" deleted"});
+  });
+}); //destroy
 
+
+// start server
 app.listen(3000, function() {
   console.log("SERVER ON!");
 });
